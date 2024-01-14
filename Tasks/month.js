7e22341d72e3908df31c0aed02519d0ffead1225
Lev <lev.bereza@gmail.com>
2024-01-14 17:32:37 +0200

    Changed MONTHS appearance and renamed function parameter

--- Tasks/month.js
+++ Tasks/month.js
@@ -2,24 +2,15 @@
 'use strict';
 
 const MONTHS = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+  'jan', 'feb', 'mar', 'apr',
+  'may', 'jun', 'jul', 'aug',
+  'sep', 'oct', 'nov', 'dec',
 ];
 
-const getMonthNumber = (s) => {
+const getMonthNumber = (month) => {
   const l = MONTHS.length;
   for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(MONTHS[i])) return i + 1;
+    if (month.toLowerCase().startsWith(MONTHS[i])) return i + 1;
   }
   return -1;
 };
