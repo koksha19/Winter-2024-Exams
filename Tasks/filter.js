47a0099d6fe740caa4b134d31fa4c2bcbd12aef4
Lev <lev.bereza@gmail.com>
2024-01-14 13:04:27 +0200

    Deleted unnecessary code.

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,11 +1,7 @@
 // Filter array by type name
 'use strict';
 
-const filterArray = (arrayToFilter, type) => {
-  const filteredArray = arrayToFilter.filter(
-    (element) => typeof element === type,
-  );
-  return filteredArray;
-};
+const filterArray = (arrayToFilter, type) =>
+  arrayToFilter.filter((element) => typeof element === type);
 
 module.exports = filterArray;
