66b7cdc2cd0e5344884b693dd7f7d91b56526355
Lev <lev.bereza@gmail.com>
2024-01-14 17:40:49 +0200

    Simplified the loop.

--- Tasks/month.js
+++ Tasks/month.js
@@ -8,12 +8,10 @@ const MONTHS = [
 ];
 
 const getMonthNumber = (month) => {
-  let index = 0;
-  for (const monthName of MONTHS) {
+  for (const [index, monthName] of MONTHS.entries()) {
     if (month.toLowerCase().startsWith(monthName)) {
       return index + 1;
     }
-    index++;
   }
   return -1;
 };
