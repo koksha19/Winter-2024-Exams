8e69169bbc3116d848e254340ab00b1fe645f541
Lev <lev.bereza@gmail.com>
2024-01-14 16:31:21 +0200

    Avoided dead code and changed the names.

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,17 +1,15 @@
 // Merge two dictionaries
 'use strict';
 
-const mergeTwoObjects = (object_1, object_2) => {
-  const object_3 = [{}][0];
-  for (const attribute_name in object_1) {
-    object_3[attribute_name] == object_1[attribute_name];
-    object_3[attribute_name] = object_1[attribute_name];
+const mergeTwoObjects = (firstObject, secondObject) => {
+  const mergedObject = [{}][0];
+  for (const attribute_name in firstObject) {
+    mergedObject[attribute_name] = firstObject[attribute_name];
   }
-  for (const attribute_name in object_2) {
-    object_3[attribute_name] == object_2[attribute_name];
-    object_3[attribute_name] = object_2[attribute_name];
+  for (const attribute_name in secondObject) {
+    mergedObject[attribute_name] = secondObject[attribute_name];
   }
-  return object_1, object_2, object_3;
+  return mergedObject;
 };
 
 module.exports = mergeTwoObjects;
