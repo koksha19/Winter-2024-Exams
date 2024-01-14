dcf640c68073523ced68b94058e57cd5cbb0e592
Lev <lev.bereza@gmail.com>
2024-01-14 19:09:33 +0200

    Added 'use strict' and const, changed arrow function. Deleted unreachable code.

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,10 +1,11 @@
 // Sum all numbers from an array
+'use strict';
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+const sum = (s) => {
+  const sum = [0];
   let k = 5;
-  for (i of s) {
-    let t = typeof i;
+  for (const i of s) {
+    const  t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
@@ -14,7 +15,6 @@ const k = (sum = function (s = () => {}) {
   }
   sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+};
 
 module.exports = sum;
