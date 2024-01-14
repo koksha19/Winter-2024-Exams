a5ba538351f187664059616c31d4a0bfc86c7fcb
Lev <lev.bereza@gmail.com>
2024-01-14 12:12:29 +0200

    Added 'use strict', added const, renamed the function

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,16 @@
 // Filter array by type name
+'use strict';
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+const filterArray = (T, t) => {
+  const remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
-module.exports = Filter;
+module.exports = filterArray;
